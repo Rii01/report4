37d0a1f6a460ce538210c9f737fed8fc5327a10e
根路銘理子 <riko_mogu@rikomogu.local>
2021-12-07 00:39:53 +0900

    LivingThing追加

--- app/src/main/java/jp/ac/uryukyu/ie/e215749/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215749/Hero.java
@@ -8,11 +8,8 @@ package jp.ac.uryukyu.ie.e215749;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Hero {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Hero extends LivingThing {
+
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -20,12 +17,8 @@ public class Hero {
      * @param maximumHP ヒーローのHP
      * @param attack ヒーローの攻撃力
      */
-    public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    public Hero  (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
     }
 
     /**
@@ -33,11 +26,7 @@ public class Hero {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param e 攻撃対象
      */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
-    }
+
 
     /**
      * 名前を取得するメソッド。
@@ -64,9 +53,7 @@ public class Hero {
      * 死ぬかどうかを取得するメソッド。
      * 
      */
-    public boolean getDead() {
-        return this.dead;
-    }
+
 
     /**
      * 名前をセットするメソッド。
@@ -102,6 +89,7 @@ public class Hero {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
